a06e5613570205db420de0399f484f5664939eb6
jnfrancisqq <jnfrancisqq@outlook.com>
2023-07-11 16:58:19 +0100

    Add project files.

---
+++ SmallestPostiveNoNotInArray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Solution
+{
+
+    public static void Main ()
+    {
+        int[] A = new int[] { 1, 2,3, 5, 7, 9 };
+
+        solution(A);
+    }
+    public static int solution(int[] A)
+    {
+
+        int smallestPostive = 1;
+
+        //sort the given array
+        Array.Sort(A);
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (A[i] <= 0)
+                continue;
+            else if (A[i] == smallestPostive)
+            {
+                smallestPostive++;
+            }
+        }
+
+        return smallestPostive;
+    }
+
+}
